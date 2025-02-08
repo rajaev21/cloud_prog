f9201306bf33aedeba166fbd0e2befcb665970e4
rajeav21 <rajaevberame21@gmail.com>
2025-02-08 13:27:54 +0800

    date

--- hello.java
+++ hello.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class hello {
 
     public static void main(String[] args) {
+
+        Date date = new Date();
         
         System.out.println("Hello, GitHub!");
+        System.out.println(date);
+
     }
 }
